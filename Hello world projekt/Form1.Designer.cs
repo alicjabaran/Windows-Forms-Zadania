9c0e8dd6ff448d5baf486aa468b5c5e97693927f
Alicja <alicja.baran0203@gmail.com>
2025-05-09 13:26:34 +0200

    git

--- Hello world projekt/Form1.Designer.cs
+++ Hello world projekt/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.guzik = new System.Windows.Forms.Button();
+            this.Hello = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // guzik
+            // 
+            this.guzik.Location = new System.Drawing.Point(265, 191);
+            this.guzik.Name = "guzik";
+            this.guzik.Size = new System.Drawing.Size(282, 67);
+            this.guzik.TabIndex = 0;
+            this.guzik.Text = "Kliknij";
+            this.guzik.UseVisualStyleBackColor = true;
+            this.guzik.Click += new System.EventHandler(this.guzik_Click);
+            // 
+            // Hello
+            // 
+            this.Hello.AutoSize = true;
+            this.Hello.Location = new System.Drawing.Point(359, 279);
+            this.Hello.Name = "Hello";
+            this.Hello.Size = new System.Drawing.Size(0, 16);
+            this.Hello.TabIndex = 1;
+            this.Hello.Click += new System.EventHandler(this.Hello_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Hello);
+            this.Controls.Add(this.guzik);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button guzik;
+        private System.Windows.Forms.Label Hello;
     }
 }
 
--- Hello world projekt/Form1.cs
+++ Hello world projekt/Form1.cs
@@ -16,5 +16,15 @@ namespace Hello_world_projekt
         {
             InitializeComponent();
         }
+
+        private void Hello_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guzik_Click(object sender, EventArgs e)
+        {
+            Hello.Text = "Hello World";
+        }
     }
 }
